f69637a29bab509eb5bdb82aa4d9fa9d3ff7f3ea
Andrzej Frysiak <fryciarz7@gmail.com>
2026-02-02 14:00:06 +0100

    initial commit

---
+++ Plugin.cs
@@ -0,0 +1,21 @@
+﻿using SPTarkov.Server.Core.Models.Spt.Mod;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CertifiedPMC;
+public record ModMetadata : AbstractModMetadata
+{
+    public override string ModGuid { get; init; } = "com.fryciarz7.spt.certpmc";
+    public override string Name { get; init; } = "Certified PMC";
+    public override string Author { get; init; } = "fryciarz7";
+    public override List<string>? Contributors { get; init; }
+    public override SemanticVersioning.Version Version { get; init; } = new("1.0.0");
+    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.11");
+
+    public override List<string>? Incompatibilities { get; init; }
+    public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
+    public override string? Url { get; init; }
+    public override bool? IsBundleMod { get; init; }
+    public override string? License { get; init; } = "Creative Commons BY-NC-SA 3.0";
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Certified PMC
+
+A mod for SP Tarkov, an offline single player emulator for the game Escape from Tarkov.
+
+This mod randomizes your PMC’s skills and masteries at character creation, because not every “trained operative” starts life the same way.
