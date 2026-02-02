6608e1169a4b4ff6b6ec782d90329d39117508d5
Andrzej Frysiak <fryciarz7@gmail.com>
2026-02-02 14:17:07 +0100

    adds main logic file

---
+++ CertifiedPMC.cs
@@ -0,0 +1,19 @@
+﻿using SPTarkov.DI.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CertifiedPMC
+{
+    [Injectable]
+    public class CertifiedPMC
+    {
+        public CertifiedPMC() { }
+
+
+        public void ModifySkills()
+        {
+            // Implementation for modifying skills goes here
+        }
+    }
+}
